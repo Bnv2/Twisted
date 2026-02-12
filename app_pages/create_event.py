53965d6c2162939cedd109190a0b44cdac0605fe
Bnv2 <bnv2.learning@gmail.com>
2026-02-13 04:02:01 +1100

    manager permission added (correct syntax) create_event.py

--- app_pages/create_event.py
+++ app_pages/create_event.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 import re
 
 def show_create_event(get_data, conn):
-    if st.session_state.user_role != "Admin" or != "manager":
+    if st.session_state.user_role != "Admin" and st.session_state.user_role != "manager":
         st.error("Access Denied.")
         st.session_state.page = "🏠 Home Dashboard"
         st.rerun()
