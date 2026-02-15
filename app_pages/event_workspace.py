9955c773e6b023e3b19a5292b0e0cafe8fb3d2fd
Bnv2 <bnv2.learning@gmail.com>
2026-02-15 14:38:11 +1100

    Update event_workspace.py

--- app_pages/event_workspace.py
+++ app_pages/event_workspace.py
@@ -16,7 +16,6 @@ def show_event_workspace(eid, get_data, db):
     """
     # --- 🛡️ 1. DATA ACQUISITION ---
     df_events = get_data("Events")
-    # df_events['Date'] = pd.to_datetime(df_events['Date'], errors='coerce')
     event_match = df_events[df_events['Event_ID'].astype(str) == str(eid)]
     
     if event_match.empty:
